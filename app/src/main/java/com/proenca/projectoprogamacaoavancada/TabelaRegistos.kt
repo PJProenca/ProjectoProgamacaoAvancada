5ee5673d45d852baaf1fb6fd6629ccfcf8777af7
PauloProenca <paulojorgeproenca@gmail.com>
2022-06-01 10:23:51 +0100

    *** Criada a tabela TabelaRegistos ***

---
+++ app/src/main/java/com/proenca/projectoprogamacaoavancada/TabelaRegistos.kt
@@ -0,0 +1,19 @@
+package com.proenca.projectoprogamacaoavancada
+
+import android.database.sqlite.SQLiteDatabase
+import android.provider.BaseColumns
+
+class TabelaRegistos(db:SQLiteDatabase):TabelasBD(db,NOME) {
+    override fun cria(){
+        db.execSQL("CREATE TABLE $NOME (${BaseColumns._ID} INTEGER PRIMARY KEY AUTOINCREMENT,$DATA_REG DATE NOT NULL,$GLICEMIA INTEGER NOT NULL,$INSULINA INTEGER NOT NULL,$PESO DOUBLE NOT NULL ,$ID_PACIENTE INTEGER NOT NULL,FOREIGN KEY ($ID_PACIENTE) REFERENCES ${TabelaPacientes.NOME}(${BaseColumns._ID}) ON DELETE RESTRICT) ")
+    }
+
+    companion object{
+        const val NOME = "Registos"
+        const val ID_PACIENTE = "ID_do_Paciente"
+        const val DATA_REG = "Data_do_Registo"
+        const val GLICEMIA="Glicemia_Medida"
+        const val INSULINA = "Unidade_Administrada"
+        const val PESO = "Peso"
+    }
+}
