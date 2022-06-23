3bcc8ee5ef5f06d042f0a8b2167b5edf5519ca6d
PauloProenca <paulojorgeproenca@gmail.com>
2022-06-23 18:26:38 +0100

    *** Criada uma funçao para converter um cursor em Alimentos ***

--- app/src/main/java/com/proenca/projectoprogamacaoavancada/Alimentos.kt
+++ app/src/main/java/com/proenca/projectoprogamacaoavancada/Alimentos.kt
@@ -1,6 +1,8 @@
 package com.proenca.projectoprogamacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Alimentos(var nome: String,
                      var hidratos : Long,
@@ -11,5 +13,18 @@ data class Alimentos(var nome: String,
         valores.put(TabelaAlimentos.C_NOME,nome)
         valores.put(TabelaAlimentos.C_VALOR,hidratos)
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor):Alimentos{
+            val id_pos = cursor.getColumnIndex(BaseColumns._ID)
+            val nome_pos = cursor.getColumnIndex(TabelaAlimentos.C_NOME)
+            val valor_pos = cursor.getColumnIndex(TabelaAlimentos.C_VALOR)
+
+            val id = cursor.getLong(id_pos)
+            val nome = cursor.getString(nome_pos)
+            val valor = cursor.getLong(valor_pos)
+            return Alimentos(nome,valor,id)
+        }
     }
 }
