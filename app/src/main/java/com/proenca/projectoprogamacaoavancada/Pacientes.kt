7166f6b9874be451f617c3c2e29a04f445e3b33b
PauloProenca <paulojorgeproenca@gmail.com>
2022-06-23 16:53:19 +0100

    *** Criado funçao para converter um cursor em Pacientes ***

--- app/src/main/java/com/proenca/projectoprogamacaoavancada/Pacientes.kt
+++ app/src/main/java/com/proenca/projectoprogamacaoavancada/Pacientes.kt
@@ -1,6 +1,8 @@
 package com.proenca.projectoprogamacaoavancada
 
 import android.content.ContentValues
+import android.database.Cursor
+import android.provider.BaseColumns
 
 data class Pacientes( var nome : String,var dataNasc: String, var altura : Long,var id:Long= -1){
 
@@ -11,5 +13,20 @@ data class Pacientes( var nome : String,var dataNasc: String, var altura : Long,
 
         valores.put(TabelaPacientes.C_ALTURA,altura)
         return valores
+    }
+
+    companion object{
+        fun fromCursor(cursor: Cursor) : Pacientes{
+            val id_Pos = cursor.getColumnIndex(BaseColumns._ID)
+            val nome_pos = cursor.getColumnIndex(TabelaPacientes.C_NOME)
+            val dNasc_pos = cursor.getColumnIndex(TabelaPacientes.C_DATA_NASC)
+            val altura_pos = cursor.getColumnIndex(TabelaPacientes.C_ALTURA)
+
+            val id = cursor.getLong(id_Pos)
+            val nome = cursor.getString(nome_pos)
+            val dataNasc = cursor.getString(dNasc_pos)
+            val altura = cursor.getLong(altura_pos)
+            return Pacientes(nome,dataNasc,altura,id)
+        }
     }
 }
