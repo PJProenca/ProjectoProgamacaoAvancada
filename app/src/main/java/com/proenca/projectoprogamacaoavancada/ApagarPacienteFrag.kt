48e43f9e21569d4f3d3b2005bc682c20e866ab0b
PauloProenca <paulojorgeproenca@gmail.com>
2022-07-03 16:02:10 +0100

    *** Criado menu e alterar o mesmo quando passamos para o fragmento ApagarPacienteFrag  ***

--- app/src/main/java/com/proenca/projectoprogamacaoavancada/ApagarPacienteFrag.kt
+++ app/src/main/java/com/proenca/projectoprogamacaoavancada/ApagarPacienteFrag.kt
@@ -2,13 +2,12 @@ package com.proenca.projectoprogamacaoavancada
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.proenca.projectoprogamacaoavancada.databinding.FragmentApagarPacienteBinding
+
 
 /**
  * A simple [Fragment] subclass.
@@ -16,40 +15,42 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ApagarPacienteFrag : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private var _binding: FragmentApagarPacienteBinding? = null
+    private val binding get() = _binding!!
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val activity = requireActivity() as MainActivity
+        activity.fragment = this
+        activity.itemAtual = R.menu.menu_apagar
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentApagarPacienteBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    fun processaOpcaoMenu(item: MenuItem): Boolean{
+        return when (item.itemId){
+            R.id.action_delete ->{
+                true
+            }
+            R.id.action_cancel ->{
+                true
+            }
+            else ->false
         }
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_apagar_paciente, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ApagarPacienteFrag.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic fun newInstance(param1: String, param2: String) =
-                ApagarPacienteFrag().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
 }
