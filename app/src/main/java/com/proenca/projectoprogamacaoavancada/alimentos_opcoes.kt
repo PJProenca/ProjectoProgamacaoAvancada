79a55cee61e491fee51dd398d33e70ce47cdfc61
PauloProenca <paulojorgeproenca@gmail.com>
2022-07-05 15:48:32 +0100

    *** Criado e inicializado o Cursorloader para ler os Alimentos ***

--- app/src/main/java/com/proenca/projectoprogamacaoavancada/alimentos_opcoes.kt
+++ app/src/main/java/com/proenca/projectoprogamacaoavancada/alimentos_opcoes.kt
@@ -7,38 +7,58 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.loader.app.LoaderManager
+import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
 import com.proenca.projectoprogamacaoavancada.databinding.FragmentAlimentosOpcoesBinding
 
 
 class alimentos_opcoes : Fragment() , LoaderManager.LoaderCallbacks<Cursor>{
 
-    private val _binding: FragmentAlimentosOpcoesBinding?=null
+    private var _binding: FragmentAlimentosOpcoesBinding?=null
+
+    private val binding get() = _binding!!
 
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        LoaderManager.getInstance(this).initLoader(ID_LOADER_ALIMENTOS, null, this)
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding=null
+    }
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alimentos_opcoes, container, false)
+
+        _binding = FragmentAlimentosOpcoesBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
+        CursorLoader(requireContext(),
+            myContentProvider.ENDERECO_ALIMENTOS,
+            TabelaAlimentos.TODAS_COLUNAS,
+            null,
+            null,
+            TabelaAlimentos.C_NOME)
+
 
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     override fun onLoaderReset(loader: Loader<Cursor>) {
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
+    }
+
+    companion object{
+        const val ID_LOADER_ALIMENTOS = 0
     }
 
 
